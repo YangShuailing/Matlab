270ca9edf7d93081fc5035283c2cd8cc1b168582
YangShuailing <yangsl1008@gmail.com>
2015-04-29 21:10:12 +0800

    4.29
    仿真图片细节修改

--- 画图/Communication complexity/CC_draw.m
+++ 画图/Communication complexity/CC_draw.m
@@ -1,14 +1,15 @@
 clc;
-x = 1:100;
-for i=1:100
+clear;
+x = 1:50;
+for i=1:50
 y(i)=RBS(i);
 y1(i)=VASL(i);
 end
 
-plot(x,y,'g-',x,y1,'b:','linewidth',1.5) 
-xlabel('The number of nodes')
+plot(x,y,'k-',x,y1,'k-.','linewidth',1.3) 
+xlabel('The munber of nodes with Level1')
 ylabel('The number of transmission data ')
-legend('RBS','VASL');
+legend('Global synchronization scheme','VASL ');
 % text(50,2500,'\rightarrow RBS')
 % text(70,895,'  VASL \rightarrow ')
 hold on 
--- 画图/Communication complexity/RBS.m
+++ 画图/Communication complexity/RBS.m
@@ -3,8 +3,11 @@ function y = RBS( x )
 %   此处显示详细说明
 if x>=0&&x<=4
     y=1;
-elseif x>4
-    y=x*x-x*sqrt(x)+rand();
+elseif 4<x && x<=12
+    y=x*x-x*sqrt(x)+rand()*10;
+elseif x>12
+    y=x*x-x*sqrt(x)+rand()*60;
+%        y=x*x-x*sqrt(x) ;
 end
 
 end
